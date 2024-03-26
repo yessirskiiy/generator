849e50230992ec5c33c3242a1e0cedbfe958abf0
Антон Езерский <anton030701@gmail.com>
2024-03-26 19:48:43 +0300

    second celery build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-celery --app=generator worker -l INFO --beat
+#celery --app=generator worker -l INFO --beat
