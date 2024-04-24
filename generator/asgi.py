71786152d6cfb7226541f96d790696c3fb35496b
Антон Езерский <anton030701@gmail.com>
2024-04-24 21:11:32 +0300

    async final build

--- generator/asgi.py
+++ generator/asgi.py
@@ -5,13 +5,15 @@ from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+django_asgi_app = get_asgi_application()
+
 from backend.routing import ws_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'generator.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(ws_urlpatterns)
     ),
 })
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'generator.settings')
